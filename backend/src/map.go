6183d893f60e9987d0fa4267d8b71bfe4a61f99d
Parth <parth@mehrotra.me>
2018-03-15 17:51:08 -0400

    more functions

--- backend/src/map.go
+++ backend/src/map.go
@@ -6,3 +6,13 @@ type Tile struct {
 type Map struct {
 	Tiles [][]Tile
 }
+
+func (m *Map) NewMap(width int, height int) *Map {
+	newMap := new(Map)
+	//TODO
+	return newMap
+}
+
+func (m *Map) spawnNewSnake(Player string, initialLen int, startingX int, startingY int) {
+
+}
--- backend/src/snake.go
+++ backend/src/snake.go
@@ -3,6 +3,7 @@ type Snake struct {
 	Length int
 	Head *SnakeNode
 	Tail *SnakeNode
+	Map Map*
 }
 
 type SnakeNode {
@@ -13,13 +14,14 @@ type SnakeNode {
 	Prev SnakeNode*
 }
 
-func (snake *Snake) NewSnake(x int, y int) *Snake {
+func (snake *Snake) NewSnake(x int, y int, world Map*) *Snake {
 	s := new(Snake)
 	s.Head = &SnakeNode{&s, x, y, nil, nil}
 	s.Tail = Head
 	s.Length = 1
+	s.Map = world
 }
 
-func (snake *Snake) move(direction int, [][][]int) {
+func (snake *Snake) move(direction int) {
 
 }
