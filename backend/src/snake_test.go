229d5b9503d7200eb50e7c2d4afa6565a8be6683
steverusso <steverusso@protonmail.com>
2018-03-16 17:12:45 -0400

    Added test.

--- backend/src/snake_test.go
+++ backend/src/snake_test.go
@@ -6,6 +6,9 @@ import (
 
 func TestNewSnake(t *testing.T) {
 	testSnake := NewSnake(0, 0, nil)
+	if testSnake.Length != 1 {
+		t.Error("expected", 1, "got", testSnake.Length)
+	}
 	if testSnake.Map != nil {
 		t.Error("expected", nil, "got", testSnake.Map)
 	}
