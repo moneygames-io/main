f961b2e83899a900d64992037c25a506c69e133c
steverusso <steverusso@protonmail.com>
2018-03-16 17:35:04 -0400

    Got the basic web server up.

--- backend/src/server.go
+++ backend/src/server.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Server starting...")
+
+	http.Handle("/", http.FileServer(http.Dir("../../frontend/src")))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("Server stoping...")
 }
