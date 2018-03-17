65abe44c3a1e9a64a16d5eae8459a359cf61205e
Parth <parth@mehrotra.me>
2018-03-17 19:54:50 -0400

    weird bug

--- backend/src/snake_test.go
+++ backend/src/snake_test.go
@@ -1,28 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 	"github.com/stretchr/testify/assert"
 )
 
 type DummyMap struct {
+	AddSN int
+	RemoveSN int
+	AddF int
+	RemoveF int
 }
 
 func (dm DummyMap) AddSnakeNode(sn *SnakeNode) {
+	dm.AddSN = dm.AddSN+1
 }
 
 func (dm DummyMap) RemoveSnakeNode (sn *SnakeNode) {
+	dm.RemoveSN++
 }
 
 func (dm DummyMap) AddFood (sn *Food) {
+	dm.AddF++
 }
 
 func (dm DummyMap) RemoveFood (sn *Food) {
+	dm.RemoveF++
 }
 
 func TestNewSnake(t *testing.T) {
-	dummyMap := DummyMap{}
+	dummyMap := DummyMap{0, 0, 0, 0}
 	testSnake := NewSnake(0, 0, dummyMap)
 	assert.Equal(t, testSnake.Length, 1, "new snake length should be 1")
 	assert.Equal(t, testSnake.CurrentDirection, 0, "default current direction should be 0")
+	fmt.Println(dummyMap)
+	assert.Equal(t, 1, dummyMap.AddSN);
 }
