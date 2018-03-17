58946b7981c4385b2b743391bc06fdabdf653034
Parth <parth@mehrotra.me>
2018-03-17 18:31:26 -0400

    first test passing

--- backend/src/snake_test.go
+++ backend/src/snake_test.go
@@ -5,8 +5,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type DummyMap struct {
+}
+
+func (dm DummyMap) AddSnakeNode(sn *SnakeNode) {
+}
+
+func (dm DummyMap) RemoveSnakeNode (sn *SnakeNode) {
+}
+
+func (dm DummyMap) AddFood (sn *Food) {
+}
+
+func (dm DummyMap) RemoveFood (sn *Food) {
+}
+
 func TestNewSnake(t *testing.T) {
-	testSnake := NewSnake(0, 0, nil)
+	dummyMap := DummyMap{}
+	testSnake := NewSnake(0, 0, dummyMap)
 	assert.Equal(t, testSnake.Length, 1, "new snake length should be 1")
 	assert.Equal(t, testSnake.CurrentDirection, 0, "default current direction should be 0")
 }
