218d522a14b4b475e499bf8a297df34fa5b065bd
root <parth@mehrotra.me>
2018-03-20 03:44:22 +0000

    added map scafolding

--- backend/src/map.go
+++ backend/src/map.go
@@ -1,13 +1,18 @@
 package main
 
+import (
+	"math/rand"
+)
+
 type Tile struct {
 	Snake *Snake
-	Depth int
+	Food *Food
 }
 
 type Map struct {
 	Tiles [][]Tile
 	Players map[string]*Snake
+	Losers map[string]*Snake
 }
 
 type MapEvent interface {
@@ -20,43 +25,73 @@ type MapEvent interface {
 }
 
 func NewMap(players int) *Map {
-	newMap := new(Map)
+	newMap := &Map{}
 	newMap.Tiles = [players * 100][players * 100]Tile{}
-	for rows := 0; rows < len(newMap.Tiles); rows++ {
-		for cols := 0; cols < len(newMap.Tiles[0]); cols++ {
-			
-		}
-	}
-
+	newMap.Players = make(map[string]*Snake)
+	newMap.Losers = make(map[string]*Snake)
 	return newMap
 }
 
 func (m *Map) SpawnNewPlayer(player string) bool {
+	row := rand.Intn(len(m.Tiles))
+	col := rand.Intn(len(m.Tiles[0]))
+
+	for m.Tiles[row][col].Snake != nil && m.Tiles[row][col].Food != nil {
+		row = rand.Intn(len(m.Tiles))
+		col = rand.Intn(len(m.Tiles[0]))
+	}
+
+	map.Players[player] = NewSnake(col, row, m)
+}
+
+func (m *Map) SnakeCreated(snake *Snake) {
+	m.Tiles[x][y].Snake = snake
+	m.Tiles[x][y].Depth++
+}
+
+func (m *Map) HeadMoved(snake *Snake) int {
+	x := snake.Head.X
+	y := snake.Head.Y
+
+	if m.Tiles[y][x].Snake != nil {
+		if snake != m.Tiles[y][x] {
+			return 2
+		}
+	}
+
+	if m.Tiles[y][x].Food != nil {
+		m.RemoveFood(m.Tiles[y][x].Food)
+		return 1
+	}
+
+	return 0
+}
+
+func (m *Map) RemoveTailNode(sn *SnakeNode) {
+	x := sn.X
+    y := sn.Y
+
+	m.Tiles[y][x].Snake = nil
+}
+
+func (m *Map) AddFood(food *Food) {
+	x := food.X
+    y := food.Y
+
+	m.Tiles[y][x] = food
+}
+
+func (m *Map) RemoveFood(food *Food) {
+	x := food.X
+    y := food.Y
+
+	m.Tiles[y][x].Food = nil
 
 }
 
-func (m *Map) SnakeCreated(*Snake) {
-
-}
-
-func (m *Map) HeadMoved(*Snake) int {
-
-}
-
-func (m *Map) RemoveTailNode(*SnakeNode) {
-
-}
-
-func (m *Map) AddFood(*Food) {
-
-}
-
-func (m *Map) RemoveFood(*Food) {
-
-}
-
-func (m *Map) SnakeRemoved(*Snake) {
-
+func (m *Map) SnakeRemoved(snake *Snake) {
+	m.Players[snake.Player] = nil
+	m.Losers[snake.Player] = snake
 }
 
 func (m *Map) tick() {
