43a9d3c4361d66393e7d6715386c01e2bbac7e8b
Parth Mehrotra <parth@mehrotra.me>
2018-05-17 12:56:52 -0400

    UI server

---
+++ frontend/src/ui-server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", rootHandler)
+
+	panic(http.ListenAndServe(":8000", nil))
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	content, err := ioutil.ReadFile("index.html")
+	if err != nil {
+		fmt.Println("Could not open file.", err)
+	}
+	fmt.Fprintf(w, "%s", content)
+}
