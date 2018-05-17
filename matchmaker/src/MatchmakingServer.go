06ec718771c849ab778b3593986bee614383ad38
Parth Mehrotra <parth@mehrotra.me>
2018-05-16 21:32:12 -0400

    externalizing

--- matchmaker/src/MatchmakingServer.go
+++ matchmaker/src/MatchmakingServer.go
@@ -1,36 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gorilla/websocket"
-	"io/ioutil"
 	"net/http"
 )
 
 var matchmaker *Matchmaker
 
 func main() {
-	matchmaker = NewMatchmaker(3)
+	matchmaker = NewMatchmaker(2)
 
 	http.HandleFunc("/ws", wsHandler)
-	http.HandleFunc("/", rootHandler)
 
-	panic(http.ListenAndServe(":8000", nil))
-}
-
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	content, err := ioutil.ReadFile("index.html")
-	if err != nil {
-		fmt.Println("Could not open file.", err)
-	}
-	fmt.Fprintf(w, "%s", content)
+	panic(http.ListenAndServe(":8001", nil))
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Header.Get("Origin") != "http://"+r.Host {
-		http.Error(w, "Origin not allowed", 403)
-		return
-	}
 	conn, err := websocket.Upgrade(w, r, w.Header(), 1024, 1024)
 	if err != nil {
 		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
