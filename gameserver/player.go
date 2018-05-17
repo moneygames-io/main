90e7436df55def7b18c0d5d2704f5d9379788a56
Parth Mehrotra <parth@mehrotra.me>
2018-05-17 15:42:06 -0400

    no src folder

---
+++ gameserver/player.go
@@ -0,0 +1,10 @@
+package main
+
+type Player struct {
+	Name             string
+	CurrentDirection int
+	CurrentZoomLevel int
+	CurrentSprint    bool
+	WalletAddress    string
+	Snake            *Snake
+}
