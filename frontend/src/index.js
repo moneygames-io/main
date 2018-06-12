b257cb66e992d456ca98123a69eb88328b07423c
Parth Mehrotra <parth@mehrotra.me>
2018-06-12 17:11:23 -0400

    frontend structure finalized

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,13 @@ import Gameserver from './gameserver.js'
 
 class Index {
 	constructor() {
-	  let matchmaker = new Matchmaker("ws://127.0.0.1:8001/ws", this.gameserverReady.bind(this));
+	  this.matchmaker = new Matchmaker("ws://127.0.0.1:8001/ws", this.gameserverReady.bind(this));
 
-	  matchmaker.joinQueue();
+	  this.matchmaker.joinQueue();
 	}
 
 	gameserverReady(gs) {
+		this.matchmaker.destroyCanvas();
 		let gameserver = new Gameserver(gs);
 		gameserver.connect();
 	}
