18e99d3a6a1769e971f931b9416848b139ec23ed
Parth Mehrotra <parth@mehrotra.me>
2018-06-30 12:44:00 -0400

    working on redis state nicely

--- gameserver-service/watcher.go
+++ gameserver-service/watcher.go
@@ -3,24 +3,31 @@ package main
 import (
 	"fmt"
 	"github.com/go-redis/redis"
+	"strconv"
 	"time"
 )
 
+var currentPort int
+
+// TODO can we just cut redis out? Can we use docker as a store of state? Do we need to use redis for messaging? Can we just hold this state in memory and just operate on Docker's state?
+
 func main() {
+	currentPort = 10000
 	client := redis.NewClient(&redis.Options{
 		Addr:     "redis:6379",
 		Password: "",
 		DB:       0,
 	})
 
-	pong, err := client.Ping().Result()
+	_, err := client.Ping().Result()
 
 	if err != nil {
-		fmt.Println(pong)
 		fmt.Println(err)
+		fmt.Println("exiting")
 		return
 	}
 
+	fmt.Println("not exiting")
 	doEvery(3*time.Second, checkRedis, client)
 }
 
@@ -30,8 +37,12 @@ func doEvery(d time.Duration, f func(*redis.Client), c *redis.Client) {
 	}
 }
 
-func addGameServer() {
-	fmt.Println("Adding Game Server")
+func addGameServer(c *redis.Client) {
+	err := c.Set(strconv.Itoa(currentPort), "idle", 0).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+	currentPort++
 }
 
 func checkRedis(c *redis.Client) {
@@ -46,6 +57,6 @@ func checkRedis(c *redis.Client) {
 	}
 
 	if idleCount < 2 {
-		addGameServer()
+		addGameServer(c)
 	}
 }
