a6d6ffd12efbdffd01b3e2f8032300c02d49c0c6
Parth Mehrotra <parth@mehrotra.me>
2018-08-02 14:40:58 -0400

    index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,18 +2,10 @@ import Matchmaker from './matchmaker.js'
 import Gameserver from './gameserver.js'
 
 class Index {
-<<<<<<< HEAD
 	constructor() {
 	  this.matchmaker = new Matchmaker("ws://" + window.location.hostname + ":8000/ws", this.gameserverReady.bind(this));
 	  this.matchmaker.joinQueue();
 	}
-=======
-    constructor() {
-        this.matchmaker = new Matchmaker("ws://" + window.location.hostname + ":8000/ws", this.gameserverReady.bind(this));
-
-        this.matchmaker.joinQueue();
-    }
->>>>>>> b3586b65ec65e049c9f5a66e7b0b7a924972b490
 
     gameserverReady(gs) {
         this.matchmaker.destroyCanvas();
